015d0e0f0e0cc3f88586f2a032f8eb47dee0faae
sanath221 <81934465+sanath221@users.noreply.github.com>
2021-05-27 19:18:15 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,10 +1,16 @@
-resource "aws_instance" "myfirstinstance" {
-    ami = "ami-00399ec92321828f5"
-    count =1
-    key_name = "test-key"
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIAS4MRDJIL7AEB6447"
+  secret_key = "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"
+}
+resource "aws_instance" "myfirstInstance" {
+    ami  = "ami-00399ec92321828f5"
+    count = 1
+    key_name = "AWS_KeyPair"
     instance_type = "t2.micro"
-    security_groups = [ "launch-wizard-2" ]
-tags={
+    
+
+tags= {
     Name = "aws_instance"
 }  
 }
